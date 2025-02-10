b2df5218e52169a0648c185ed54940b57d1cfd31
Jadimatic <Jadimatic@gmail.com>
2025-02-10 15:46:12 -0800

    Pushing for review, trying to make blocks drop their respective items.

--- src/main/java/io/github/jadimatic/jadis_mended_progression/JadisMendedProgressionItems.java
+++ src/main/java/io/github/jadimatic/jadis_mended_progression/JadisMendedProgressionItems.java
@@ -19,10 +19,8 @@ public class JadisMendedProgressionItems {
 
 	//Block Fields
 	public static final Block CHARCOAL_BLOCK = new Block(QuiltBlockSettings.create()
-		.requiresTool()
 		.hardness(1.0f));
 	public static final Block EXAMPLE_BLOCK = new Block(QuiltBlockSettings.create()
-		.requiresTool()
 		.hardness(0.5f));
 	//End Of Fields
 
